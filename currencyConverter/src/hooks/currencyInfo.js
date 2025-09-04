bfaef2e5812f92980ca14f8ddb2ec6a42a5e6752
ankit saini <sainiankit21981@gmail.com>
2025-09-05 00:12:36 +0530

    currency converter project

---
+++ currencyConverter/src/hooks/currencyInfo.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+function UseCurrencyInfo(currency) {
+  const [data, setData] = useState({}); //i wnat to show change in ui and want to get data in a aaray
+  useEffect(() => {
+     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+    .then((res) => res.json())
+    .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+export default UseCurrencyInfo;
